7a5b0841e064c743d0ae678675177fb6371cf542
Tobias <tobias.jansson@gmail.com>
2025-12-02 18:19:35 +0100

    dag 2 del 1

--- 02.js
+++ 02.js
@@ -3,27 +3,27 @@ const args = process.argv.slice(2)
 
 const data = (fs.readFileSync(args[0], 'utf8'))
 const lines = data.split('\n')
-
 const ranges = lines[0].split(',').map(r => {
-    const match = r.match(/(\d+)-(\d+)/)
-    if (match) {
-        const min = match[1]
-        const max = match[2]
-        return { min, max }
-    }
-    return { min: '', max: '' }
+    const match = r.split('-')
+    return { min: match[0], max: match[1] }
 })
 
+let part1 = 0
 ranges.forEach(range => {
     const min = parseInt(range.min)
     const max = parseInt(range.max)
-    const rangeLength = max - min
-    for (let i = 0; i <= rangeLength; i++) {
+    for (let i = min; i <= max; i++) {
         const num = i.toString()
         const seq1 = num.substring(0, num.length / 2)
         const seq2 = num.slice(-num.length / 2)
-        if(seq1 === seq2) {
-            console.log(`Found matching sequence: ${num} in range ${range.min}-${range.max}`)
+        // console.log(`Comparing ${num} ${seq1} to ${seq2}`)
+
+        // Bara jämna annars blir det jättedåligt
+        if(seq1 === seq2 && num.length % 2 === 0) {
+          //  console.log(`Found matching sequence: ${num} in range ${range.min}-${range.max}`)
+            part1 += i
         }
     }
 })
+
+console.log(`Part 1: ${part1}`)
