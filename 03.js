bbd1ac2385de3343807c61bf3b72055cdfb6cfec
Tobias <tobias.jansson@gmail.com>
2025-12-03 07:31:51 +0100

    dag 3 del 1

--- 03.js
+++ 03.js
@@ -2,4 +2,19 @@ const fs = require('fs')
 const args = process.argv.slice(2)
 
 const data = (fs.readFileSync(args[0], 'utf8'))
-const lines = data.split('\n')
+const banks = data.split('\n')
+
+let totalJoltage = 0
+banks.forEach(bank => {
+    const jolts = bank.split('')
+    let max = 0
+    for(let i = 0; i < jolts.length; i++) {
+        for(let j = i + 1; j < jolts.length; j++) {
+            const joltage = parseInt(jolts[i] + jolts[j])
+            if(joltage > max) max = joltage
+        }
+    }
+    totalJoltage += max
+})
+
+console.log('Del 1:', totalJoltage)
