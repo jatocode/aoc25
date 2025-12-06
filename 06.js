1dc6c0cf34baefc0b56555f3d97f1d8e394aa2e8
Tobias <tobias.jansson@gmail.com>
2025-12-06 11:58:28 +0100

    Dag 6 del 2

--- 06.js
+++ 06.js
@@ -4,27 +4,67 @@ const args = process.argv.slice(2)
 const data = (readFileSync(args[0], 'utf8'))
 const lines = data.split('\n')
 
+console.log(`Part 1: `, del1(lines))
+console.log(`Part 2: `, del2(lines))
 
-let lastline = lines[lines.length - 1]
-let ops = lastline.trim().split(/\s+/)
-    .map(c => {
-        if (c === '+') return (a, b) => a + b
-        if (c === '*') return (a, b) => {
-            a = a === 0 ? 1 : a
-            b = b === 0 ? 1 : b
-            return a * b
+function del1(lines) {
+    let lastline = lines[lines.length - 1]
+    let ops = lastline.trim().split(/\s+/)
+        .map(c => {
+            if (c === '+') return add()
+            if (c === '*') return multiply()
+        })
+    let columns = []
+    for (let i = 0; i < lines.length - 1; i++) {
+        const data = lines[i].trim().split(/\s+/).map(n => parseInt(n))
+        for (let j = 0; j < data.length; j++) {
+            if (!columns[j]) columns[j] = 0
+            columns[j] = ops[j](columns[j], data[j])
         }
-})
-let columns = []
-for(let i = 0; i < lines.length-1; i++) {
-    const data = lines[i].trim().split(/\s+/).map(n => parseInt(n))
-    for(let j = 0; j < data.length; j++) {
-        if(!columns[j]) columns[j] = 0
-        columns[j] = ops[j](columns[j], data[j])
+    }
+    return columns.reduce((a, b) => a + b, 0)
+}
+
+function del2(lines) {
+    let lastline = lines[lines.length - 1].split('')
+    let currentop = ''
+    let problem = 0
+    let problems = []
+    for (let i = 0; i < lastline.length; i++) {
+        const curr = getColumn(lines, i)
+        if (curr > 0) {
+            if (lastline[i] === '+') currentop = add()
+            else if (lastline[i] === '*') currentop = multiply()
+            problem = currentop(problem, curr)
+        } else {
+            problems.push(problem)
+            problem = 0
+        }
+    }
+    problems.push(problem)
+    return problems.reduce((a, b) => a + b, 0)
+}
+
+function getColumn(lines, index) {
+    let column = []
+    for (let i = 0; i < lines.length - 1; i++) {
+        const row = lines[i].split('')
+        const digit = parseInt(row[index])
+        if (digit > 0) column.push(digit)
+    }
+    return parseInt(column.join(''))
+}
+
+function add() {
+    return (a, b) => a + b
+}
+
+function multiply() {
+    return (a, b) => {
+        a = a === 0 ? 1 : a
+        b = b === 0 ? 1 : b
+        return a * b
     }
 }
-let part1 = columns.reduce((a,b) => a + b, 0)
 
-console.log(`Part 1: `, part1)
-//console.log(`Part 2: `, part2)
 
