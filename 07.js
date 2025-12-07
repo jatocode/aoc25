723002c1c44fa03c870b870b74178e574ea96944
Tobias <tobias.jansson@gmail.com>
2025-12-07 19:26:37 +0100

    dag 7 del 2

--- 07.js
+++ 07.js
@@ -5,16 +5,21 @@ const data = (readFileSync(args[0], 'utf8'))
 const lines = data.split('\n')
 
 let field = []
-lines.forEach(line => {
-    field.push(line.split(''))
-})
-const start = { x: lines[0].indexOf('S'), y: 1 }
-const part1 = calcbeam(start.x, start.y)
-//printfield()
-console.log(`Part 1: `, part1)
+lines.forEach(line => field.push(line.split('')))
+
+const x = lines[0].indexOf('S')
+const calc = calcbeam(x, 1)
+console.log(`Part 1: `, calc)
+
+const beams = []
+beams[x] = 1
+const times = calctimes()
+let part2 = times
+console.log(`Part 2: `, part2)
 
 function calcbeam(x,y) {
     let splits = 0
+
     while (y < field.length) {
         if (field[y][x] === '.') {
             field[y][x] = '|'
@@ -23,7 +28,7 @@ function calcbeam(x,y) {
             splits += calcbeam(x - 1, y + 1)
             splits += calcbeam(x + 1, y + 1)
             break
-        } else if (field[y][x] === '|') {   
+        }  else if (field[y][x] === '|') {   
             break
         }
         y++
@@ -31,7 +36,25 @@ function calcbeam(x,y) {
     return splits
 }
 
-function printfield() {
+function calctimes() {
+    for (let y = 0; y < field.length; y++) {
+        for (let x = 0; x < field[y].length; x++) {
+            if (beams[x] === undefined || beams[x] === 0) continue
+
+            if (field[y][x] === '^') {
+             //   console.log(`Beam at (${x},${y}) splits ${beams[x]} times`)
+                if (beams[x - 1] === undefined) beams[x - 1] = 0
+                if (beams[x + 1] === undefined) beams[x + 1] = 0
+                beams[x - 1] += beams[x]
+                beams[x + 1] += beams[x]
+                beams[x] = 0 // döda strålen här
+            }
+        }
+    }
+    return beams.reduce((a,b) => a + b, 0)
+}
+
+function printfield(field) {
     field.forEach(line => {
         console.log(line.join(''))
     })
